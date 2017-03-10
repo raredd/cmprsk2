e56e0b0c0350e35b4843c87deb717f4fbbbc8eb0
Robert Redd <raredd01@gmail.com>
2017-03-10 12:18:03 -0500

    general maintenance - improve examples, tests, docs

---
+++ tests/testthat/test-ic.R
@@ -0,0 +1,27 @@
+context('information criterion')
+
+test_that('IC aliases return identical results', {
+  
+  crr <- crr2(Surv(futime, event(censored) == death) ~ age + sex + abo,
+              transplant)[[1L]]
+  
+  expect_identical(
+    AIC(crr), extractAIC(crr)
+  )
+  expect_identical(
+    AIC(crr), extractIC(crr, 'AIC')
+  )
+  
+  expect_identical(
+    BIC(crr), extractIC(crr, 'BIC')
+  )
+  
+  expect_identical(
+    -2 * crr$loglik, unname(extractIC(crr, p = 0))
+  )
+  
+  expect_identical(
+    logLik(crr)[[1]], crr$loglik
+  )
+  
+})
