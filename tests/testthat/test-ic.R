77e9fc045d2da52f5de6e79a3d34999f948a666c
Robert Redd <raredd01@gmail.com>
2017-03-17 12:19:39 -0400

    ic tests - ignore names in output

--- tests/testthat/test-ic.R
+++ tests/testthat/test-ic.R
@@ -5,22 +5,31 @@ test_that('IC aliases return identical results', {
   crr <- crr2(Surv(futime, event(censored) == death) ~ age + sex + abo,
               transplant)[[1L]]
   
-  expect_identical(
+  ## ignore names in results
+  expect_identical_unnamed <- function(object, expected, ...) {
+    testthat::expect_identical(
+      unname(object),
+      unname(expected),
+      ...
+    )
+  }
+  
+  expect_identical_unnamed(
     AIC(crr), extractAIC(crr)
   )
-  expect_identical(
+  expect_identical_unnamed(
     AIC(crr), extractIC(crr, 'AIC')
   )
   
-  expect_identical(
+  expect_identical_unnamed(
     BIC(crr), extractIC(crr, 'BIC')
   )
   
-  expect_identical(
-    -2 * crr$loglik, unname(extractIC(crr, p = 0))
+  expect_identical_unnamed(
+    -2 * crr$loglik, extractIC(crr, p = 0)
   )
   
-  expect_identical(
+  expect_identical_unnamed(
     logLik(crr)[[1L]], crr$loglik
   )
   
