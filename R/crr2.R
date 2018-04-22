5257dd478dd425f573bc579fe405832fb9d5f6db
Robert Redd <raredd01@gmail.com>
2018-04-22 13:17:39 -0400

    typo

--- R/crr2.R
+++ R/crr2.R
@@ -171,7 +171,7 @@ crr2 <- function(formula, data, which = NULL, cox = FALSE, variance = TRUE,
   
   formula <- sprintf('Surv(%s, %s == %s) ~ %s',
                      lhs[1L], lhs[2L], shQuote(failcode),
-                     paste0(deparse(formula[[3L]])))
+                     paste(deparse(formula[[3L]]), collapse = ''))
   formula <- as.formula(formula)
   
   ## add model.frame for use in other methods
@@ -238,7 +238,7 @@ crr2 <- function(formula, data, which = NULL, cox = FALSE, variance = TRUE,
   ## coxph model with any competing event vs censored
   formula <- sprintf('Surv(%s, %s %%in%% c(%s)) ~ %s',
                      lhs[1L], lhs[2L], toString(shQuote(crisks)),
-                     paste0(deparse(formula[[3L]])))
+                     paste(deparse(formula[[3L]]), collapse = ''))
   formula <- as.formula(formula)
   
   cph <- substitute(coxph(formula, data), list(formula = formula))
