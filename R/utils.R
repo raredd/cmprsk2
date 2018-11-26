48d6b8ec2bd09d2a78f125fbc72ee1c0cb90fe28
Robert Redd <raredd01@gmail.com>
2018-11-26 16:43:36 -0500

    pvalr - update based on rawr::pvalr, set journal = TRUE as default

--- R/utils.R
+++ R/utils.R
@@ -162,28 +162,32 @@ parse_formula <- function(formula, data = NULL, name = NULL) {
   )
 }
 
-pvalr <- function(pvals, sig.limit = 0.001, digits = 3L,
-         html = FALSE, show.p = FALSE) {
+pvalr <- function(pv, sig.limit = 0.001, digits = 3L, html = FALSE,
+                  show.p = FALSE, journal = TRUE, ...) {
   ## rawr::pvalr
   stopifnot(
     sig.limit > 0,
     sig.limit < 1
   )
+  
   show.p <- show.p + 1L
   html   <- html + 1L
   
-  sapply(pvals, function(x, sig.limit) {
+  sapply(pv, function(x, sig.limit) {
     if (is.na(x) | !nzchar(x))
       return(NA)
     if (x >= 0.99)
-      return(paste0(c('','p ')[show.p], c('> ','&gt; ')[html], '0.99'))
-    if (x >= 0.9)
-      return(paste0(c('','p ')[show.p], c('> ','&gt; ')[html], '0.9'))
+      return(paste0(c('', 'p ')[show.p], c('> ', '&gt; ')[html], '0.99'))
+    if (x >= 0.9 && !journal)
+      return(paste0(c('', 'p ')[show.p], c('> ', '&gt; ')[html], '0.9'))
     if (x < sig.limit) {
-      paste0(c('', 'p ')[show.p], c('< ', '&lt; ')[html], format(sig.limit))
+      paste0(c('', 'p ')[show.p], c('< ', '&lt; ')[html],
+             format.pval(sig.limit, ...))
     } else {
-      nd <- c(digits, 2L, 1L)[findInterval(x, c(-Inf, .1, .5, Inf))]
-      paste0(c('','p = ')[show.p], roundr(x, nd))
+      nd <- if (journal)
+        c(digits, 2L)[findInterval(x, c(-Inf, .1, Inf))]
+      else c(digits, 2L, 1L)[findInterval(x, c(-Inf, .1, .5, Inf))]
+      paste0(c('', 'p = ')[show.p], roundr(x, nd))
     }
   }, sig.limit)
 }
